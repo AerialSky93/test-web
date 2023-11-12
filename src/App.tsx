99929f1471830c9a41c896bc1f20996d092fcd11
AerialSky93 <rdutta23@yahoo.com>
2023-11-11 17:26:05 -0800

    create headers

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import CustomerTable from "./components/customer-table/customer-table";
 import CustomerForm from "./components/customer-form/customer-form";
+import Header from "./Header";
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/getAll" element={<CustomerTable />} />
-      <Route path="/post" element={<CustomerForm />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/getAll" element={<CustomerTable />} />
+        <Route path="/post" element={<CustomerForm />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Header.tsx
@@ -0,0 +1,80 @@
+import "./App.css";
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Link,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { AccountCircle } from "@mui/icons-material";
+import { useState } from "react";
+function Header() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <Box sx={{ flexGrow: 1 }} marginBottom={5}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            id="demo-positioned-menu"
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleClick}
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="demo-positioned-menu"
+            aria-labelledby="demo-positioned-button"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+          >
+            <MenuItem onClick={handleClose} component={Link} href="/getall">
+              View All Data
+            </MenuItem>
+            <MenuItem onClick={handleClose} component={Link} href="/post">
+              Create Data
+            </MenuItem>
+            <MenuItem onClick={handleClose}>Update Item</MenuItem>
+          </Menu>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Tools
+          </Typography>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            color="inherit"
+          >
+            <AccountCircle />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default Header;
