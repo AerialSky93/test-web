9528c041ddbe613eecedd89e169090643c9fd533
AerialSky93 <rdutta23@yahoo.com>
2023-11-12 12:44:16 -0800

    change host

--- src/service-api/customer-service.tsx
+++ src/service-api/customer-service.tsx
@@ -1,6 +1,8 @@
 import { CustomerCreateRequest } from "./dto/customer-create-request";
 
-export const customerURL = "http://localhost:8080/api/customer";
+//export const customerURL = "http://localhost:8080/api/customer";
+
+export const customerURL = "https://testdataapp.azurewebsites.net/api/customer";
 
 export function customerGetData(): Promise<Response> {
   return fetch(customerURL, { method: "GET" });
