1bbd16c3e4a4d1b9a0e74e32a5fee89cb90e47c6
AerialSky93 <rdutta23@yahoo.com>
2023-11-14 10:48:26 -0800

    add job routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Home";
-import CustomerTable from "./components/customer-table/customer-table";
-import CustomerForm from "./components/customer-form/customer-form";
 import Header from "./Header";
+import JobPostingForm from "./components/job-posting-form/job-posting-form";
+import JobPostingTable from "./components/job-posting-table/job-posting-table";
 
 function App() {
   return (
@@ -11,8 +11,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/getAll" element={<CustomerTable />} />
-        <Route path="/post" element={<CustomerForm />} />
+        <Route path="/getAll" element={<JobPostingTable />} />
+        <Route path="/post" element={<JobPostingForm />} />
       </Routes>
     </>
   );
